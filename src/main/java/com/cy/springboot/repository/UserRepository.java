02465630ceed9a4c1815a0e8e31fd2ad52b8cf23
zjtu <zjtu721@163.com>
2022-03-26 11:17:56 +0800

    更新

--- src/main/java/com/cy/springboot/repository/UserRepository.java
+++ src/main/java/com/cy/springboot/repository/UserRepository.java
@@ -2,11 +2,19 @@ package com.cy.springboot.repository;
 
 import com.cy.springboot.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
 
+    @Modifying
+    @Query("update User set username=:username  where id=:id")
+    @Transactional
+    int updatePasswordByUsername(@Param("username") String username, @Param("id") Integer id);
 
 }
